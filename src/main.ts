9224e4ba1b7c995ede8a667f62f295b34f2a21fc
Annika Egger <annikaegger83@gmail.com>
2026-07-30 18:25:48 +0200

    style: styling the preview infobar

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,39 @@
-import './scss/main.scss';
+import "./scss/main.scss";
 let theme = "code vibes";
 
-init()
+init();
 
 function init() {
-const fieldRef = document.getElementById("field");
-if(fieldRef) {
-    fieldRef.addEventListener("click", e => {
-        const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement
-        if(card) {
-            card.classList.toggle("is-flipped")
-        }
-    })
-}
+  const fieldRef = document.getElementById("field");
+  if (fieldRef) {
+    fieldRef.addEventListener("click", (e) => {
+      const card = (e.target as HTMLElement).closest(
+        ".card",
+      ) as HTMLButtonElement;
+      if (card) {
+        card.classList.toggle("is-flipped");
+      }
+    });
+  }
 }
 
+const fieldsets = document.querySelectorAll(".game-settings__group");
 
-// mit feature branch arbeiten!
+fieldsets.forEach((fieldset) => {
+  fieldset.addEventListener("change", (event) => {
+    if (fieldset.id === "game-themes") {
+      const target = event.target as HTMLInputElement;
+      selectTheme(target.value);
+      styleGamePreview();
+    } else if (fieldset.id === "players") {
+    } else if (fieldset.id === "board-sizes") {
+    }
+  });
+});
+
+function selectTheme(selectedTheme: string) {
+  theme = selectedTheme;
+  console.log(theme);
+}
+
+function styleGamePreview() {}
