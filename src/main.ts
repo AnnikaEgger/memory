44baf26006ce5c5f41e55d8413276697efc59c2f
Annika Egger <annikaegger83@gmail.com>
2026-07-31 16:17:14 +0200

    feat: user can now select the theme, player color and board size in settings, selected settings are being displayed correctly

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./scss/main.scss";
 let theme = "code vibes";
+let playerColor = "blue";
+let amountOfCards = 16;
 
 init();
 
@@ -21,22 +23,45 @@ const fieldsets = document.querySelectorAll(".game-settings__group");
 
 fieldsets.forEach((fieldset) => {
   fieldset.addEventListener("change", (event) => {
-    if (fieldset.id === "game-themes") {
-      const target = event.target as HTMLInputElement;
-      selectTheme(target.value);
-      styleGamePreview();
-    } else if (fieldset.id === "players") {
-    } else if (fieldset.id === "board-sizes") {
-    }
+    const target = event.target as HTMLInputElement;
+
+    if (fieldset.id === "game-themes")
+      selectTheme(target.value as "code vibes" | "gaming");
+    else if (fieldset.id === "players")
+      selectPlayer(target.value as "blue" | "orange");
+    else if (fieldset.id === "board-sizes")
+      selectBoardSize(target.value as "16" | "24" | "32");
   });
 });
 
-function selectTheme(selectedTheme: string) {
+function selectTheme(selectedTheme: "code vibes" | "gaming") {
   theme = selectedTheme;
   const body = document.querySelector("body");
   if (body) {
     body.dataset.theme = theme;
   }
+  updateSelectedTheme();
 }
 
-function styleGamePreview() {}
+function updateSelectedTheme() {
+  const selectedThemeRef = document.getElementById("chosen-theme");
+  if (selectedThemeRef) {
+    if (theme === "code vibes") selectedThemeRef.innerText = "Code Vibes Theme";
+    else selectedThemeRef.innerText = "Game Theme";
+  }
+}
+
+function selectPlayer(selectedPlayer: "blue" | "orange") {
+  const selectedPlayerRef = document.getElementById("chosen-player");
+  playerColor = selectedPlayer;
+  if (selectedPlayerRef)
+    selectedPlayerRef.textContent =
+      playerColor[0].toUpperCase() + playerColor.slice(1) + " Player";
+}
+
+function selectBoardSize(selectedBoardSize: "16" | "24" | "32") {
+  const selectedSizeRef = document.getElementById("chosen-board-size");
+  amountOfCards = Number(selectedBoardSize);
+  if (selectedSizeRef)
+    selectedSizeRef.textContent = "Board-" + amountOfCards + " Cards";
+}
